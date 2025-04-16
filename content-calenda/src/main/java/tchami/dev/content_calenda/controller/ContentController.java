30d4133851886a9d1e80b5ae02de30fb407b2640
tchami <131756888+tchamianest@users.noreply.github.com>
2025-04-16 22:10:41 +0200

    Feat:make controller getter for single with id and all

--- content-calenda/src/main/java/tchami/dev/content_calenda/controller/ContentController.java
+++ content-calenda/src/main/java/tchami/dev/content_calenda/controller/ContentController.java
@@ -1,9 +1,33 @@
 package tchami.dev.content_calenda.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import tchami.dev.content_calenda.model.Content;
+import tchami.dev.content_calenda.repository.ContentCollectionRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
+    private final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository){
+        this.repository =repository;
+    }
+
+//    make the route visible to the app level
+    @GetMapping("")
+    public List<Content> findAll(){
+        return  repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id){
+        return repository.finById(id).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"CONTENT NOT FOUND"));
+    }
+
 }
